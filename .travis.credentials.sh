8a4080d9856746215c08bca21699ee34be2b664a
Max Hohenegger <github.com@hohenegger.eu>
2015-02-23 00:11:55 +0100

    switching from sbt to curl

--- .travis.credentials.sh
+++ .travis.credentials.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-mkdir ~/.bintray/
-FILE=$HOME/.bintray/.credentials
-cat <<EOF >$FILE
-realm = Bintray API Realm
-host = api.bintray.com
-user = $BINTRAY_USER
-password = $BINTRAY_API_KEY
-EOF
-echo $BINTRAY_USER
-echo "Created ~/.bintray/.credentials file: Here it is: "
-ls -la $FILE 
+
+#mkdir ~/.bintray/
+#FILE=$HOME/.bintray/.credentials
+#cat <<EOF >$FILE
+#realm = Bintray API Realm
+#host = api.bintray.com
+#user = $BINTRAY_USER
+#password = $BINTRAY_API_KEY
+#EOF
+#echo $BINTRAY_USER
+#echo "Created ~/.bintray/.credentials file: Here it is: "
+#ls -la $FILE 
+
+curl -T winlogger/target/winlogger*.jar -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/$BINTRAY_USER/generic/winlogger/$TRAVIS_BUILD_NUMBER/
